2c3fd65097b985e6cf1d3af24d60a85c5a313cf6
RosarioElissalde <130700778+RosarioElissalde@users.noreply.github.com>
2023-05-26 14:16:03 -0300

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,12 +1,11 @@
 function saludo() {
-    var nombre = prompt("Por favor, ingresa tu nombre");
-    
-    if (nombre) {
-      alert("¡Bienvenid@, " + nombre + "!");
-    } else {
-      alert("Por favor, ingresa tu nombre.");
+    var nombre = prompt ("Por favor, ingresa tu nombre");
+  
+    while (!nombre) {
+      nombre = prompt("Por favor, ingresa tu nombre");
     }
-    console.log ("Bienvenido " + nombre + "!");
+  
+    alert("¡Bienvenido, " + nombre + "!");
   }
   
   saludo();
